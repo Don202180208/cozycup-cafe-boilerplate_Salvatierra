c1248f884e212ef466cd605e9967a70099bb0ef6
Don202180208 <202180208@psu.palawan.edu.ph>
2024-01-26 11:54:46 +0800

    Home page 1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Add any JavaScript functionality here if needed
 document.getElementById("newsletterForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    // Add your newsletter subscription logic here
     alert("Subscribed to the newsletter!");
 });
